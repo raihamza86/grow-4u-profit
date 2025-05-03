0c5ff0eb8dc94b5c38c99d6492a62fe808860908
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-05-03 16:10:40 +0500

    Add links

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,9 +3,7 @@ import Hero from "../components/home/Hero";
 import Brand from "../components/home/Brand";
 import Certificate from "../components/home/Certificate";
 import Offers from "../components/Offers";
-import Brand from '../components/home/Brand';
-import Certificate from '../components/home/Certificate';
-import Expert from '../components/home/Expert';
+import Expert from "../components/home/Expert";
 
 const Home = () => {
   return (
@@ -14,15 +12,9 @@ const Home = () => {
       <Brand />
       <Certificate />
       <Offers />
+      <Expert />
     </div>
   );
 };
-    return (
-        <div>
-            <Hero />
-            <Brand/>
-            <Certificate/>
-            <Expert/>
-            </div>
 
 export default Home;
