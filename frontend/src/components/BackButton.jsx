a10daee0a30f8e893ccb93d003fee4614517cabe
raihamza86 <rahamza146@gmail.com>
2025-05-10 11:07:55 +0500

    make dashboard sidebar responsive

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-const BackButton = ({ showDashNav  }) => {
+const BackButton = ({ showDashNav }) => {
   const navigate = useNavigate();
 
   const MovetoDashboard = () => {
@@ -12,8 +12,8 @@ const BackButton = ({ showDashNav  }) => {
     <div
       className="text-green-500 text-xl hover:text-blue-500 font-bold mt-5 cursor-pointer"
       onClick={() => {
-        showDashNav();  
-        MovetoDashboard();      
+        // showDashNav();
+        MovetoDashboard();
       }}
     >
       Back Now
